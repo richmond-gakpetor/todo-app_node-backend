b1bec791f0f83638642f9152f71b12da340ad1e4
VISION <gakpetorr01@gmail.com>
2021-12-28 18:26:08 +0000

    handled base url

--- app.js
+++ app.js
@@ -6,14 +6,18 @@ const todoController = require("./controllers/todoController");
 
 app.use(express.json());
 
+app.get("/", function (req, res) {
+  res.status(200).json({ message: "Welcome to isaa-todo-API" });
+});
 app.get("/todos", todoController.getAllTodo);
 app.post("/todos", todoController.addTodo);
 app.delete("/todos/:todoId", todoController.deleteTodoById);
 app.patch("/todos/:todoId", todoController.updateTodoById);
 app.get("/todos/:todoId", todoController.getTodoById);
+const PORT = process.env.PORT || 3000;
 
 // listener
-app.listen(3000, function () {
+app.listen(PORT, function () {
   console.log("Server has started to run...");
   mongoose
     .connect(process.env.DB_URL)
